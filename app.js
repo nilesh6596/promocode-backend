221c70206decd0ca309000c2d34726d02da302d0
NileshSolanki9678 <nilesh.solanki@ikoble.com>
2022-09-30 10:39:17 +0530

    fix: cors error

--- app.js
+++ app.js
@@ -8,20 +8,21 @@ const cors = require('cors');
 
 const app = express();
 
-app.options('*', cors({
-    origin: '*',
-    preflightContinue: true,
-    methods: 'POST, GET, PUT, DELETE, OPTIONS',
-    optionsSuccessStatus: 204,
-    allowedHeaders: ['Origin, X-Requested-With, Content-Type, Accept, Authorization, token, orgName, user_code, File-Name'],
-    exposedHeaders: ['Authorization, File-Name']
-}))
+// app.options('*', cors({
+//     origin: '*',
+//     preflightContinue: true,
+//     methods: 'POST, GET, PUT, DELETE, OPTIONS',
+//     optionsSuccessStatus: 204,
+//     allowedHeaders: ['Origin, X-Requested-With, Content-Type, Accept, Authorization, token, orgName, user_code, File-Name'],
+//     exposedHeaders: ['Authorization, File-Name']
+// }))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- models/promotionCode.js
+++ models/promotionCode.js
@@ -4,7 +4,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 require('mongoose-long')(mongoose);
-const SchemaTypes = mongoose.Schema.Types;
 const {
     autoIncrement
 } = require('../lib/utils');
